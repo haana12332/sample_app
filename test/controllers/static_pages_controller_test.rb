3a41ed41ee27a37980bfae8c85c810e944f6c253
arai <160373144+haana12332@users.noreply.github.com>
2026-05-24 09:16:02 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,13 +6,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get home' do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     assert_select 'title', "Home | #{@base_title}"
   end
 
   test 'should get help' do
-    get static_pages_help_url
+    get root_url
     assert_response :success
     assert_select 'title', "Help | #{@base_title}"
   end
@@ -21,5 +21,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select 'title', "About | #{@base_title}"
+  end
+
+  test 'should get contact' do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select 'title', "Contact | #{@base_title}"
   end
 end
